21944458cf294f3cc86cf8c3dd333c0e33d14da1
wkadmin <wkadmin@jdmollard.local>
2024-08-07 13:52:59 -0600

    upd - InSAR plot 4944683

--- scripts/NRL_sf.R
+++ scripts/NRL_sf.R
@@ -477,8 +477,8 @@ ogoki_2016 %>%
 ### 4.04 Insar dates on combined hyeto-hydrographs ----
 
 ### InSAR station subset
-insar_7641 <- insar_tidy %>%
-  filter(id %in% "4937641")
+insar_4683 <- insar_tidy %>%
+  filter(id %in% "4944683")
 
 ### Combined plot
 lansdowne_hist <- lansdowne_clean %>% 
@@ -491,7 +491,7 @@ lansdowne_hist <- lansdowne_clean %>%
   scale_y_reverse(limits = c(100, 0)) +
   
   # Annotations
-  geom_vline(data = insar_7641, aes(xintercept = date, colour = "salmon"), linetype = "dashed") +
+  geom_vline(data = insar_4683, aes(xintercept = date, colour = "salmon"), linetype = "dashed") +
   
   # Theme
   theme_light() +
@@ -499,7 +499,7 @@ lansdowne_hist <- lansdowne_clean %>%
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank(),
         legend.position = "none") +
-  ggtitle("Historical Attawapiskat River discharge | Weather station: Lansdowne House (AUT) | InSAR ID: 4937641")
+  ggtitle("Historical Attawapiskat River discharge | Weather station: Lansdowne House (AUT) | InSAR ID: 4944683")
 
 
 attawa_hist_q <- attawapiskat_exp %>%
@@ -515,9 +515,9 @@ attawa_hist_q <- attawapiskat_exp %>%
   ylab(expression(paste("Discharge ", (m^3/s)))) +
   
   # Annotations
-  geom_vline(data = insar_7641, aes(xintercept = date, colour = "salmon"), linetype = "dashed") +
-  geom_text(data = insar_7641,
-            mapping = aes(x = date - 18, y = 1600, label = date,),
+  geom_vline(data = insar_4683, aes(xintercept = date, colour = "salmon"), linetype = "dashed") +
+  geom_text(data = insar_4683,
+            mapping = aes(x = date - 20, y = 1600, label = date,),
             inherit.aes = FALSE,
             size = 3,
             hjust = 1,
@@ -535,9 +535,9 @@ lansdowne_hist / attawa_hist_q
 
 ### 4.05 Raw Insar profile combined with corresponding sf and precip
 
-### InSAR 7641
-insar7641_plot <- insar_long %>% 
-  filter(point_id %in% "4937641") %>% 
+### InSAR 4683
+insar4683_plot <- insar_long %>% 
+  filter(point_id %in% "4944683") %>% 
   ggplot(aes(x = date, y = displacement)) +
   
   # Point plot
@@ -548,13 +548,13 @@ insar7641_plot <- insar_long %>%
   scale_x_date(limits = date_range, date_labels = "%Y", date_breaks = "1 year") +
   
   # Annotations
-  geom_vline(data = insar_7641, aes(xintercept = date, colour = "salmon"), linetype = "dashed") +
+  geom_vline(data = insar_4683, aes(xintercept = date, colour = "salmon"), linetype = "dashed") +
   
   # Theme
   theme_light() +
   theme(legend.position = "none")
 
-lansdowne_hist / attawa_hist_q / insar7641_plot + plot_layout(guides = "collect")
+lansdowne_hist / attawa_hist_q / insar4683_plot + plot_layout(guides = "collect")
 
 ## 5. SAVING // EXPORTING ----
 
